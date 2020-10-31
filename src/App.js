bfeac1a64a16e3f63e6e4ff5687dda9efae6c96f
krishnabharambe <krishnabharambe@gmail.com>
2020-11-01 01:07:05 +0530

    added contact info

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 function App() {
   return (
     <div>
-<b>This site is still under construction...</b>
-
-Please. contact administrator for more info.
-
-https://github.com/krishnabharambe ot mail us at : krishnabharambe@hotmail.com
+      <b>This site is still under construction...</b>
+      <br />
+        Please. contact administrator for more info.
+      <br />
+        https://github.com/krishnabharambe or mail us at : krishnabharambe@hotmail.com
     </div>
   );
 }
