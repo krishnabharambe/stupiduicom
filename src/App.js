dae467fec906cf4db6b53a1dd601dd9814f4e5d5
krishnabharambe <krishnabharambe@gmail.com>
2020-11-01 21:00:15 +0530

    pre added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 function App() {
   return (
     <div>
-      <b>This site is still under construction...</b>
-      <br />
+      <pre>
+        <b>This site is still under construction...</b>
+        <br />
         Please. contact administrator for more info.
       <br />
         https://github.com/krishnabharambe or mail us at : krishnabharambe@hotmail.com
+        </pre>
     </div>
   );
 }
